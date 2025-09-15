3948aea712cbc7bafeeb3d76802c820e1ac86d18
Laura <lauraamanda56@gmail.com>
2025-09-15 23:37:04 +0100

    updated tf files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -94,6 +94,22 @@ resource "aws_security_group" "k8s_cluster" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP (for website)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS (for website)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Rancher HTTP (custom port)
   ingress {
     from_port   = 8081
